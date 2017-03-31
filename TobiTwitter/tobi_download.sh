6bfad1fe7eadc3795a1da6055cd8a7fe7554b027
aryasobn <aryasobn@mail.uc.edu>
2017-03-31 13:11:25 -0400

    script for streaming the data from twitter

---
+++ TobiTwitter/tobi_download.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#cd ~/home/arya_b/TobiTwitter/2download/gold/
+python /home/arya_b/TobiTwitter/twitter_download/download_tweets_api.py --dist=/home/arya_b/TobiTwitter/2download/gold/train/100_topics_XXX_tweets.topic-two-point.subtask-BD.train.gold.txt --output=downloaded.train.tsv
+#python /home/arya_b/TobiTwitter/twitter_download/download_tweets_api.py --dist=/home/arya_b/TobiTwitter/2download/gold/devtest/100_topics_XXX_tweets.topic-two-point.subtask-BD.devtest.gold.txt --output=downloaded.devtest.tsv
+#python /home/arya_b/TobiTwitter/twitter_download/download_tweets_api.py --dist=/home/arya_b/TobiTwitter/2download/gold/dev/100_topics_XXX_tweets.topic-two-point.subtask-BD.dev.gold.txt --output=downloaded.dev.tsv
+
